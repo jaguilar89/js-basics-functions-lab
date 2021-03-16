9c957cd91a01093f28062f1a729e7ee2755f4048
Jose Aguilar <jaguilarny@gmail.com>
2021-03-15 22:06:01 -0400

    commit tree 5c6ccdfed8611d99c96020258ff2a05fcf827d45

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(startingLocation) {
+    if (startingLocation >= 42) {
+        return (startingLocation - 42)
+    } else if (startingLocation < 42) {
+        return (42 - startingLocation)
+    }
+};
+
+function distanceFromHqInFeet(startingLocation) {
+    return (startingLocation - 42) * 264
+}
+
+function distanceTravelledInFeet(startingPoint, destination) {
+    if (startingPoint < destination) {
+    return (destination - startingPoint) * 264
+} else {
+    return (startingPoint - destination) * 264
+}
+};
